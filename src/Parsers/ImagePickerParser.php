b7acd494888d983e43f742b2290858292194bad8
Rick Wouters <rick@happytechnology.nl>
2022-08-24 13:41:34 +0200

    feat: wrote tests, improved ImagePickerParser

--- src/Parsers/ImagePickerParser.php
+++ src/Parsers/ImagePickerParser.php
@@ -5,22 +5,34 @@ declare(strict_types=1);
 namespace Collecthor\SurveyjsParser\Parsers;
 
 use Collecthor\SurveyjsParser\ElementParserInterface;
-use Collecthor\SurveyjsParser\Parsers\SingleChoiceQuestionParser;
+use Collecthor\SurveyjsParser\ParserHelpers;
 use Collecthor\SurveyjsParser\SurveyConfiguration;
+use Collecthor\SurveyjsParser\Variables\MultipleChoiceVariable;
+use Collecthor\SurveyjsParser\Variables\SingleChoiceVariable;
+
+use function iter\map;
+use function iter\toArray;
 
 class ImagePickerParser implements ElementParserInterface
 {
-    public function __construct(
-        private SingleChoiceQuestionParser $singleChoiceParser,
-        private MultipleChoiceParser $multipleChoiceParser
-    ) {
-    }
+    use ParserHelpers;
     public function parse(ElementParserInterface $root, array $questionConfig, SurveyConfiguration $surveyConfiguration, array $dataPrefix = []): iterable
     {
-        if (is_bool($questionConfig['multiSelect']) && $questionConfig['multiSelect']) {
-            yield from $this->multipleChoiceParser->parse($root, $questionConfig, $surveyConfiguration, $dataPrefix);
+        /** @var list<array<string, string>> $rawChoices */
+        $rawChoices = $questionConfig['choices'];
+        $name = $this->extractName($questionConfig);
+        $titles = $this->extractTitles($questionConfig, $surveyConfiguration);
+        $choices = $this->extractChoices(
+            toArray(map(fn ($choice) => $choice['value'], $rawChoices)),
+            $surveyConfiguration
+        );
+        $valueName = $this->extractValueName($questionConfig);
+
+
+        if (isset($questionConfig['multiSelect']) && is_bool($questionConfig['multiSelect']) && $questionConfig['multiSelect']) {
+            yield new MultipleChoiceVariable($name, $titles, $choices, [...$dataPrefix, $valueName]);
         } else {
-            yield from $this->singleChoiceParser->parse($root, $questionConfig, $surveyConfiguration, $dataPrefix);
+            yield new SingleChoiceVariable($name, $titles, $choices, [...$dataPrefix, $valueName]);
         }
     }
 }
---
+++ tests/Parsers/ImagePickerParserTest.php
@@ -0,0 +1,91 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Collecthor\SurveyjsParser\Tests\Parsers;
+
+use Collecthor\SurveyjsParser\Parsers\DummyParser;
+use Collecthor\SurveyjsParser\Parsers\ImagePickerParser;
+use Collecthor\SurveyjsParser\SurveyConfiguration;
+use Collecthor\SurveyjsParser\Variables\MultipleChoiceVariable;
+use Collecthor\SurveyjsParser\Variables\SingleChoiceVariable;
+use PHPUnit\Framework\TestCase;
+
+use function iter\toArray;
+
+/**
+ * @covers \Collecthor\SurveyjsParser\Parsers\ImagePickerParser
+ * @uses \Collecthor\SurveyjsParser\Variables\MultipleChoiceVariable
+ * @uses \Collecthor\SurveyjsParser\Values\StringValueOption
+ * @uses \Collecthor\SurveyjsParser\Values\IntegerValueOption
+ * @uses \Collecthor\SurveyjsParser\Traits\GetDisplayValue
+ * @uses \Collecthor\SurveyjsParser\SurveyConfiguration
+ * @uses \Collecthor\SurveyjsParser\Variables\OpenTextVariable
+ */
+final class ImagePickerParserTest extends TestCase
+{
+    public function testParseSingleImagePicker(): void
+    {
+        $surveyConfig = new SurveyConfiguration(locales:['default', 'nl']);
+        $questionConfig = [
+            "type" => "imagepicker",
+            "name" => "question3",
+            "choices" => [
+                [
+                    "value" => "lion",
+                    "imageLink" => "https://surveyjs.io/Content/Images/examples/image-picker/lion.jpg"
+                ],
+                [
+                    "value" => "giraffe",
+                    "imageLink" => "https://surveyjs.io/Content/Images/examples/image-picker/giraffe.jpg"
+                ],
+                [
+                    "value" => "panda",
+                    "imageLink" => "https://surveyjs.io/Content/Images/examples/image-picker/panda.jpg"
+                ],
+                [
+                    "value" => "camel",
+                    "imageLink" => "https://surveyjs.io/Content/Images/examples/image-picker/camel.jpg"
+                ]
+            ]
+        ];
+
+        $parser = new ImagePickerParser();
+        $result = toArray($parser->parse(new DummyParser(), $questionConfig, $surveyConfig));
+
+        self::assertInstanceOf(SingleChoiceVariable::class, $result[0]);
+    }
+
+    public function testParseMultipleImagePicker(): void
+    {
+        $surveyConfig = new SurveyConfiguration(locales:['default', 'nl']);
+        $questionConfig = [
+            "type" => "imagepicker",
+            "name" => "question3",
+            "choices" => [
+                [
+                    "value" => "lion",
+                    "imageLink" => "https://surveyjs.io/Content/Images/examples/image-picker/lion.jpg"
+                ],
+                [
+                    "value" => "giraffe",
+                    "imageLink" => "https://surveyjs.io/Content/Images/examples/image-picker/giraffe.jpg"
+                ],
+                [
+                    "value" => "panda",
+                    "imageLink" => "https://surveyjs.io/Content/Images/examples/image-picker/panda.jpg"
+                ],
+                [
+                    "value" => "camel",
+                    "imageLink" => "https://surveyjs.io/Content/Images/examples/image-picker/camel.jpg"
+                ],
+            ],
+            "multiSelect" => true,
+        ];
+
+        $parser = new ImagePickerParser();
+        $result = toArray($parser->parse(new DummyParser(), $questionConfig, $surveyConfig));
+
+        self::assertInstanceOf(MultipleChoiceVariable::class, $result[0]);
+    }
+}
