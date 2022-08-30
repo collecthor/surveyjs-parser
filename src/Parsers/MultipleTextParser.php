e9b3a8ef43b886b99cab2231f9c369406c29d60d
Rick Wouters <rick@happytechnology.nl>
2022-08-30 13:32:26 +0200

    feat: create multiple text parser

---
+++ src/Parsers/MultipleTextParser.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Collecthor\SurveyjsParser\Parsers;
+
+use Collecthor\SurveyjsParser\ElementParserInterface;
+use Collecthor\SurveyjsParser\ParserHelpers;
+use Collecthor\SurveyjsParser\SurveyConfiguration;
+use Collecthor\SurveyjsParser\Variables\OpenTextVariable;
+
+final class MultipleTextParser implements ElementParserInterface
+{
+    use ParserHelpers;
+    public function parse(ElementParserInterface $root, array $questionConfig, SurveyConfiguration $surveyConfiguration, array $dataPrefix = []): iterable
+    {
+        $itemNames = [];
+        /** @var list<array<string, string>> $items */
+        $items = $questionConfig['items'];
+        foreach ($items as $item) {
+            $fullPath = [...$dataPrefix, $this->extractValueName($questionConfig), $item['name']];
+            $itemName = implode('.', $fullPath);
+            if (in_array($itemName, $itemNames, true)) {
+                throw new \RuntimeException("Duplicate question code: {$itemName}");
+            }
+            yield new OpenTextVariable($itemName, $this->extractTitles($item, $surveyConfiguration), $fullPath);
+            $itemNames[] = $itemName;
+        }
+    }
+}
---
+++ tests/Parsers/MultipleTextParserTest.php
@@ -0,0 +1,111 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Collecthor\SurveyjsParser\Tests\Parsers;
+
+use Collecthor\SurveyjsParser\Parsers\DummyParser;
+use Collecthor\SurveyjsParser\Parsers\MultipleTextParser;
+use Collecthor\SurveyjsParser\SurveyConfiguration;
+use Collecthor\SurveyjsParser\Variables\OpenTextVariable;
+use PHPUnit\Framework\TestCase;
+
+use function iter\toArray;
+
+/**
+ * Every question generates maxRowCount rows
+ * @covers \Collecthor\SurveyjsParser\Parsers\MultipleTextParser
+ * @uses \Collecthor\SurveyjsParser\Variables\MultipleChoiceVariable
+ * @uses \Collecthor\SurveyjsParser\Variables\SingleChoiceVariable
+ * @uses \Collecthor\SurveyjsParser\Values\StringValueOption
+ * @uses \Collecthor\SurveyjsParser\Values\IntegerValueOption
+ * @uses \Collecthor\SurveyjsParser\Traits\GetDisplayValue
+ * @uses \Collecthor\SurveyjsParser\SurveyConfiguration
+ * @uses \Collecthor\SurveyjsParser\Variables\OpenTextVariable
+ */
+final class MultipleTextParserTest extends TestCase
+{
+    public function testGetRightAmountOfVariables(): void
+    {
+        $surveyConfig = new SurveyConfiguration(locales: ['default', 'nl']);
+        $questionConfig = [
+            "type" => "multipletext",
+            "name" => "question4",
+            "items" => [
+                [
+                    "name" => "text1"
+                ],
+                [
+                    "name" => "text2"
+                ]
+            ]
+        ];
+
+        $parser = new MultipleTextParser();
+        $result = toArray($parser->parse(new DummyParser(), $questionConfig, $surveyConfig));
+
+        self::assertCount(2, $result);
+    }
+
+    public function testGetTranslations(): void
+    {
+        $surveyConfig = new SurveyConfiguration(locales: ['nl', 'default']);
+        $questionConfig = [
+            "type" => "multipletext",
+            "name" => "question4",
+            "items" => [
+                [
+                    "name" => "text1",
+                    "title" => [
+                        "nl" => "tekst1",
+                        "default" => "text1",
+                    ],
+                ],
+                [
+                    "name" => "text2",
+                    "title" => [
+                        "nl" => "tekst2",
+                        "default" => "text2",
+                    ],
+                ],
+            ],
+        ];
+        $parser = new MultipleTextParser();
+        /** @var list<OpenTextVariable> $result */
+        $result = toArray($parser->parse(new DummyParser(), $questionConfig, $surveyConfig));
+
+        self::assertSame('text1', $result[0]->getTitle());
+        self::assertSame('tekst1', $result[0]->getTitle('nl'));
+    }
+
+    public function testGetRightVariableType(): void
+    {
+        $surveyConfig = new SurveyConfiguration(locales: ['nl', 'default']);
+        $questionConfig = [
+            "type" => "multipletext",
+            "name" => "question4",
+            "items" => [
+                [
+                    "name" => "text1",
+                    "title" => [
+                        "nl" => "tekst1",
+                        "default" => "text1",
+                    ],
+                ],
+                [
+                    "name" => "text2",
+                    "title" => [
+                        "nl" => "tekst2",
+                        "default" => "text2",
+                    ],
+                ],
+            ],
+        ];
+        $parser = new MultipleTextParser();
+        $result = toArray($parser->parse(new DummyParser(), $questionConfig, $surveyConfig));
+
+        foreach ($result as $variable) {
+            self::assertInstanceOf(OpenTextVariable::class, $variable);
+        }
+    }
+}
