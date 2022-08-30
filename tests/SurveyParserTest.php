b01a3da16f47a3b02150d96b2298ca350ec8a8b1
Rick Wouters <rick@happytechnology.nl>
2022-08-30 09:53:40 +0200

    fix: fix tests

--- tests/SurveyParserTest.php
+++ tests/SurveyParserTest.php
@@ -21,6 +21,10 @@ use function iter\toArray;
  * @uses \Collecthor\SurveyjsParser\Parsers\CommentParser
  * @uses \Collecthor\SurveyjsParser\Parsers\TextQuestionParser
  * @uses \Collecthor\SurveyjsParser\Parsers\SingleChoiceQuestionParser
+ * @uses \Collecthor\SurveyjsParser\Parsers\BooleanParser
+ * @uses \Collecthor\SurveyjsParser\Parsers\ImageFeedbackParser
+ * @uses \Collecthor\SurveyjsParser\Parsers\MatrixDynamicParser
+ * @uses \Collecthor\SurveyjsParser\ParserLocalizer
  */
 class SurveyParserTest extends TestCase
 {
