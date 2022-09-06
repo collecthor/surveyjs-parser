198e0c3e53bb30261c57f478522049673f91ed1b
Rick Wouters <rick@happytechnology.nl>
2022-09-06 16:48:28 +0200

    feat: write parser for dynamic panel questions

---
+++ src/Parsers/DynamicPanelParser.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Collecthor\SurveyjsParser\Parsers;
+
+use Collecthor\SurveyjsParser\ElementParserInterface;
+use Collecthor\SurveyjsParser\ParserHelpers;
+use Collecthor\SurveyjsParser\SurveyConfiguration;
+
+final class DynamicPanelParser implements ElementParserInterface
+{
+    use ParserHelpers;
+
+    /**
+     * @param array<string, string> $rowLabels
+     */
+    public function __construct(private array $rowLabels)
+    {
+    }
+
+    public function parse(ElementParserInterface $root, array $questionConfig, SurveyConfiguration $surveyConfiguration, array $dataPrefix = []): iterable
+    {
+        $limit = $questionConfig['maxPanelCount'] ?? 10;
+
+        for ($r = 0; $r < $limit; $r++) {
+            foreach ((array)($questionConfig['templateElements'] ?? []) as $element) {
+                /** @var array<string, mixed> $rowElement */
+                $rowElement = $element;
+                $valueName = $this->extractValueName($rowElement);
+                $rowElement['title'] = $this->arrayFormat($surveyConfiguration, $valueName, " ", $this->rowLabels, " ", (string)$r);
+                $rowElement['name'] = implode('.', [...$dataPrefix, $valueName, (string)$r]);
+                yield from $root->parse($root, $rowElement, $surveyConfiguration, $dataPrefix);
+            }
+        }
+    }
+}
---
+++ tests/Parsers/DynamicPanelParserTest.php
@@ -0,0 +1,128 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Collecthor\SurveyjsParser\Tests\Parsers;
+
+use Collecthor\SurveyjsParser\ElementParserInterface;
+use Collecthor\SurveyjsParser\Parsers\DynamicPanelParser;
+use Collecthor\SurveyjsParser\Parsers\TextQuestionParser;
+use Collecthor\SurveyjsParser\SurveyConfiguration;
+use PHPUnit\Framework\TestCase;
+
+use function iter\toArray;
+
+/**
+ * @covers \Collecthor\SurveyjsParser\Parsers\DynamicPanelParser
+ * @uses \Collecthor\SurveyjsParser\Values\StringValueOption
+ */
+final class DynamicPanelParserTest extends TestCase
+{
+    public function testInvokeRootParser(): void
+    {
+        $surveyConfig = new SurveyConfiguration();
+        $questionConfig = [
+            "type" => "paneldynamic",
+            "name" => "question1",
+            "templateElements" => [
+                [
+                    "type" => "text",
+                    "name" => "question2"
+                ],
+                [
+                    "type" => "comment",
+                    "name" => "question3"
+                ]
+                ],
+            "maxPanelCount" => 1,
+        ];
+        $rootParser = $this->createMock(ElementParserInterface::class);
+        $rootParser->expects(self::exactly(2))->method('parse');
+        $parser = new DynamicPanelParser(['default' => 'row']);
+
+        $result = toArray($parser->parse($rootParser, $questionConfig, $surveyConfig));
+    }
+
+    public function testElementNames(): void
+    {
+        $surveyConfig = new SurveyConfiguration();
+        $questionConfig = [
+            "type" => "paneldynamic",
+            "name" => "question1",
+            "templateElements" => [
+                [
+                    "type" => "text",
+                    "name" => "question2"
+                ],
+                [
+                    "type" => "comment",
+                    "name" => "question3"
+                ]
+                ],
+            "maxPanelCount" => 2,
+        ];
+        $rootParser = $this->createMock(ElementParserInterface::class);
+        $rootParser->expects(self::exactly(4))->method('parse')->withConsecutive(
+            [
+                self::anything(),
+                self::callback(function ($columnConfig) {
+                    return $columnConfig['name'] === "question2.0";
+                }),
+                self::anything(),
+                self::anything()
+            ],
+            [
+                self::anything(),
+                self::callback(function ($columnConfig) {
+                    return $columnConfig['name'] === "question3.0";
+                }),
+                self::anything(),
+                self::anything()
+            ],
+            [
+                self::anything(),
+                self::callback(function ($columnConfig) {
+                    return $columnConfig['name'] === "question2.1";
+                }),
+                self::anything(),
+                self::anything()
+            ],
+            [
+                self::anything(),
+                self::callback(function ($columnConfig) {
+                    return $columnConfig['name'] === "question3.1";
+                }),
+                self::anything(),
+                self::anything()
+            ],
+        );
+        $parser = new DynamicPanelParser(['default' => 'row']);
+
+        $result = toArray($parser->parse($rootParser, $questionConfig, $surveyConfig));
+    }
+
+    public function testRootParserResult(): void
+    {
+        $surveyConfig = new SurveyConfiguration();
+        $questionConfig = [
+            "type" => "paneldynamic",
+            "name" => "question1",
+            "templateElements" => [
+                [
+                    "type" => "text",
+                    "name" => "question2"
+                ],
+                [
+                    "type" => "comment",
+                    "name" => "question3"
+                ]
+                ],
+            "maxPanelCount" => 2,
+        ];
+        $rootParser = new TextQuestionParser();
+        $parser = new DynamicPanelParser(['default' => 'row']);
+
+        $result = toArray($parser->parse($rootParser, $questionConfig, $surveyConfig));
+        self::assertSame("question2 row 0", $result[0]->getTitle());
+    }
+}
