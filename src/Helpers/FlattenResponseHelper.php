bbaf86b6b892ca8a075cb8020b5b06e6ce94ca4d
Rick Wouters <rick@happytechnology.nl>
2022-09-14 08:58:43 +0200

    feat: create flattenresponsehelper, flattenresponseinterface

---
+++ src/Helpers/FlattenResponseHelper.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Collecthor\SurveyjsParser\Helpers;
+
+use Collecthor\DataInterfaces\RecordInterface;
+use Collecthor\DataInterfaces\VariableSetInterface;
+use Collecthor\SurveyjsParser\FlattenResponseInterface;
+
+final class FlattenResponseHelper implements FlattenResponseInterface
+{
+    public function __construct(private VariableSetInterface $variables, private ?string $locale = null)
+    {
+    }
+
+    /** @param iterable<RecordInterface> $records */
+    public function flattenAll(iterable $records): iterable
+    {
+        foreach ($records as $record) {
+            foreach ($this->variables->getVariables() as $variable) {
+                yield [ $variable->getTitle($this->locale) => $variable->getDisplayValue($record, $this->locale)->getRawValue()];
+            }
+        }
+    }
+}
