ee07d115982cbea81cedf2377c8fdf625f7c1812
Rick Wouters <rick@happytechnology.nl>
2022-09-29 14:25:53 +0200

    feat: allow localization of extractName

--- src/ParserHelpers.php
+++ src/ParserHelpers.php
@@ -125,14 +125,21 @@ trait ParserHelpers
 
         return $config[$key];
     }
-
     /**
      * @param array<mixed> $config
+     * @param string $locale
+     * @return string
+     * @throws InvalidArgumentException
      */
-    private function extractName(array $config): string
+    private function extractName(array $config, string $locale = 'default'): string
     {
         if (!isset($config['name']) || !is_string($config['name'])) {
             throw new InvalidArgumentException("Expected to find a string at key `name`, inside: " . print_r($config, true));
+        }
+        if (is_array($config['name'])) {
+            if (isset($config['name'][$locale]) && is_string($config['name'][$locale])) {
+                return $config['name'][$locale];
+            }
         }
         return $config['name'];
     }
