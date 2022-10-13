c7452d124996066102da05a672f6ffe3ca6ce57b
Sam Mousa <sam@mousa.nl>
2022-10-13 12:53:31 +0200

    fix: use parserhelper, allow missing elements in panel

--- src/Parsers/PanelParser.php
+++ src/Parsers/PanelParser.php
@@ -5,19 +5,16 @@ declare(strict_types=1);
 namespace Collecthor\SurveyjsParser\Parsers;
 
 use Collecthor\SurveyjsParser\ElementParserInterface;
+use Collecthor\SurveyjsParser\ParserHelpers;
 use Collecthor\SurveyjsParser\SurveyConfiguration;
 
 class PanelParser implements ElementParserInterface
 {
+    use ParserHelpers;
     public function parse(ElementParserInterface $root, array $questionConfig, SurveyConfiguration $surveyConfiguration, array $dataPrefix = []): iterable
     {
-        if (!isset($questionConfig['elements'])) {
-            throw new \InvalidArgumentException('Elements key must be set and non-null');
-        }
-        if (!is_iterable($questionConfig['elements'])) {
-            throw new \InvalidArgumentException('Elements must be iterable, got: ' . print_r($questionConfig['elements'], true));
-        }
-        foreach ($questionConfig['elements'] as $elementConfig) {
+        foreach ($this->extractOptionalArray($questionConfig, 'elements') ?? [] as $elementConfig) {
+            /** @phpstan-ignore-next-line  */
             yield from $root->parse($root, $elementConfig, $surveyConfiguration, $dataPrefix);
         }
     }
